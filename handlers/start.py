47df250587b06c933055906102b3975a78ea4c59
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-09 22:14:24 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-strt_router = Router
+strt_router = Router()
 
 @strt_router.message(Command("start"))
 async def cmd_start(message: Message):
