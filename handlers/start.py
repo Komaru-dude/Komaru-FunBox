bc7c3c07c28d37ee3eb409815b41b3e50e008124
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-09 22:10:46 +0700

    Начинаем работу

---
+++ handlers/start.py
@@ -0,0 +1,9 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+strt_router = Router
+
+@strt_router.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.reply("Привет!\nЭтот бот на данный момент находится в разработке, данная команда будет сделана позже")
---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio, logging, os
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from handlers.start import strt_router
+
+load_dotenv()
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+token = os.getenv("BOT_API_TOKEN")
+bot = Bot(token)
+# Диспетчер
+dp = Dispatcher()
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.include_routers(strt_router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
