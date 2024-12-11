ccb1c50e90ecca7e2de298f51f50c3631f6ecf13
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-12 00:02:57 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -7,7 +7,5 @@ strt_router = Router()
 @strt_router.message(Command("start"))
 async def cmd_start(message: Message):
     await message.reply("Привет!\n"
-                        "На данный момент доступны эти команды:\n\n"
-                        "/start - Ты ввёл её совсем недавно\n"
-                        "/new_year - Отсчёт до нового года\n"
-                        "/birthdays - Отсчёты до дней рождения кошек\n")
+                        "Это развлекательный бот созданный для кочон подвала.\n"
+                        "Если хочешь узнать более подробную информацию о командах: /help")
