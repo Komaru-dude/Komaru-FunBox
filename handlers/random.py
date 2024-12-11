184bb7479bb6dfaaf995c2bde4914fe9645d0360
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-12 00:21:09 +0700

    Update random.py

--- handlers/random.py
+++ handlers/random.py
@@ -7,22 +7,16 @@ random_router = Router()
 
 @random_router.message(Command("random"))
 async def cmd_random(message: Message):
-    parts = message.text.split()
-    
-    # Если после команды нет дополнительных аргументов, просто выводим случайный ответ
-    if len(parts) == 1:
-        responses = [
-            "Да, без сомнений!",
-            "Нет, это не сбудется.",
-            "Возможно, ты прав.",
-            "Скорее всего, да.",
-            "Попробуй снова позже.",
-            "Определенно нет.",
-            "Я бы сказал да.",
-        ]
-        response = random.choice(responses)
-    else:
-        # В случае, если после команды есть аргументы
-        response = " ".join(parts[1:])
+
+    responses = [
+        "Да, без сомнений!",
+        "Нет, это не сбудется.",
+        "Возможно, ты прав.",
+        "Скорее всего, да.",
+        "Попробуй снова позже.",
+        "Определенно нет.",
+        "Я бы сказал да.",
+    ]
+    response = random.choice(responses)
     
     await message.answer(response)
