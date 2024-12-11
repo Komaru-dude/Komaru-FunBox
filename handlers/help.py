736b0a2784aae46c945a6df06ea5760c72e149c0
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-12 00:03:13 +0700

    Create help.py

---
+++ handlers/help.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+help_router = Router()
+
+@help_router.message(Command("help"))
+async def cmd_help(message: Message):
+    text = message.text.strip()
+    parts = text.split()
+
+    if len(parts) > 1:
+        argument = parts[1].lower()  # Приводим к нижнему регистру для удобства
+        if argument == "new_year":
+            await message.reply("")
