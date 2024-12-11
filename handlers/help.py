a0c5f58a21844993c9c559c7a07090b807ef71a0
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-12 00:04:51 +0700

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -12,4 +12,10 @@ async def cmd_help(message: Message):
     if len(parts) > 1:
         argument = parts[1].lower()  # Приводим к нижнему регистру для удобства
         if argument == "new_year":
-            await message.reply("")
+            await message.reply("Данная команда позволяет узнать время для нового года.\n"
+                                "Команда в качестве аргумента принимает 'krsk' - будет использованно красноярское время.")
+        elif argument == "birthdays":
+            await message.reply("Данная команда позволяет узнать когда деньрождения у множества кошек.\n"
+                                "Команда в качестве аргумента принимает 'krsk' - будет использованно красноярское время")
+        else:
+            await message.reply("Команда требует аргументы для использования, например: /help new_year")
