8c158407ce5335c44a30b15cdb03b3ec2fd23614
Komaru-dude <honkhonkovic842@gmail.com>
2024-12-18 11:38:35 +0700

    Update rpcommand.py

--- bot/handlers/rpcommand.py
+++ bot/handlers/rpcommand.py
@@ -15,28 +15,40 @@ async def text(message: Message):
     if not db.user_exists(user_id):
         db.add_user(user_id)
     if not db.user_have_username(user_id):
-        db.add_username(user_id, username=username)
+        db.add_username(user_id, username=username.lstrip('@') if username else None)
     if not db.user_have_first_name(user_id):
         db.add_first_name(user_id, first_name)
 
     if message.text and 'сжечь' in message.text.lower():
         parts = message.text.split()
-        
-        if len(parts) > 1:
-            target_username = parts[1].lstrip('@')
-            if not db.get_user_id_by_username(target_username):
-                await message.reply("Синтаксис некорректен, используйте 'сжечь @пользователь' или ответьте на сообщение пользователя")
-                return
-            else:
-                target_id = db.get_user_id_by_username(target_username)
-                target_first_name = db.get_first_name_by_id(target_id)
-                
-                # Создаём ссылку на профили обоих пользователей
-                profile_link = f"tg://user?id={user_id}"
-                clickable_name_user1 = f"<a href='{profile_link}'>{first_name}</a>"
+        if message.reply_to_message:
+            target_user_id = message.reply_to_message.from_user.id
+            target_first_name = message.reply_to_message.from_user.first_name
+            # Создаём ссылку на профили обоих пользователей
+            profile_link = f"tg://user?id={user_id}"
+            clickable_name_user1 = f"<a href='{profile_link}'>{first_name}</a>"
 
-                target_profile_link = f"tg://user?id={target_id}"
-                clickable_name_user2 = f"<a href='{target_profile_link}'>{target_first_name}</a>"
+            target_profile_link = f"tg://user?id={target_id}"
+            clickable_name_user2 = f"<a href='{target_profile_link}'>{target_first_name}</a>"
+        
+            # Формируем сообщение с эмодзи и ссылками
+            await message.answer(f"🔥 {clickable_name_user1} сжёг {clickable_name_user2}", parse_mode=ParseMode.HTML)
+        else:
+            if len(parts) > 1:
+                target_username = parts[1].lstrip('@')
+                if not db.get_user_id_by_username(target_username):
+                    await message.reply("Синтаксис некорректен, используйте 'сжечь @пользователь' или ответьте на сообщение пользователя")
+                    return
+                else:
+                    target_id = db.get_user_id_by_username(target_username)
+                    target_first_name = db.get_first_name_by_id(target_id)
                 
-                # Формируем сообщение с эмодзи и ссылками
-                await message.answer(f"🔥 {clickable_name_user1} сжёг {clickable_name_user2}", parse_mode=ParseMode.HTML)
+                    # Создаём ссылку на профили обоих пользователей
+                    profile_link = f"tg://user?id={user_id}"
+                    clickable_name_user1 = f"<a href='{profile_link}'>{first_name}</a>"
+
+                    target_profile_link = f"tg://user?id={target_id}"
+                    clickable_name_user2 = f"<a href='{target_profile_link}'>{target_first_name}</a>"
+                
+                    # Формируем сообщение с эмодзи и ссылками
+                    await message.answer(f"🔥 {clickable_name_user1} сжёг {clickable_name_user2}", parse_mode=ParseMode.HTML)
