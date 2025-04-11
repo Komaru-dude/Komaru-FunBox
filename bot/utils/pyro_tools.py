c4e6b6c5a8ba6d75d217c3098214348d8dcca01f
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-11 20:48:15 +0700

    Более геморный рестарт

--- bot/utils/pyro_tools.py
+++ bot/utils/pyro_tools.py
@@ -1,9 +1,10 @@
-import asyncio, os, logging
+import asyncio, os, logging, signal
 from pyrogram import Client
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()
+
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 token = os.getenv("BOT_API_TOKEN")
@@ -16,7 +17,6 @@ if not token:
     raise ValueError("Ошибка: переменная BOT_API_TOKEN не найдена. Убедитесь, что она присутствует в .env файле.")
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=token) if not os.path.exists("my_bot.session") else Client("my_bot")
-
 server = FastAPI()
 
 @server.get("/user/{username}")
@@ -30,7 +30,6 @@ async def get_user_id(username: str):
 @server.get("/username/{chat_id}/{user_id}")
 async def get_username_by_id(chat_id: str, user_id: int):
     try:
-        # Перебираем участников чата
         async for member in app.get_chat_members(chat_id):
             if member.user.id == user_id:
                 return {"username": member.user.username}
@@ -41,7 +40,6 @@ async def get_username_by_id(chat_id: str, user_id: int):
 @server.get("/first_name/{chat_id}/{user_id}")
 async def get_first_name_by_id(chat_id: str, user_id: int):
     try:
-        # Перебираем участников чата
         async for member in app.get_chat_members(chat_id):
             if member.user.id == user_id:
                 return {"first_name": member.user.first_name}
@@ -50,10 +48,32 @@ async def get_first_name_by_id(chat_id: str, user_id: int):
         return {"error": str(e)}
 
 async def start_pyrogram():
-    """ Запуск Pyrogram-бота в фоне """
+    logging.info("Запуск Pyrogram клиента...")
     await app.start()
-    logging.info("Pyrogram бот запущен.")
+    logging.info("Pyrogram клиент запущен.")
     await asyncio.Event().wait()
 
+async def shutdown():
+    logging.info("Остановка Pyrogram клиента...")
+    await app.stop()
+    logging.info("Pyrogram клиент остановлен.")
+
+def handle_exit(*args):
+    loop.create_task(shutdown())
+    for task in asyncio.all_tasks(loop):
+        task.cancel()
+
 loop = asyncio.get_event_loop()
+
+for sig in (signal.SIGINT, signal.SIGTERM):
+    loop.add_signal_handler(sig, handle_exit)
+
 loop.create_task(start_pyrogram())
+
+try:
+    loop.run_forever()
+except asyncio.CancelledError:
+    pass
+finally:
+    loop.run_until_complete(loop.shutdown_asyncgens())
+    loop.close()
