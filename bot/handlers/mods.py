f06654ddd3d4eb95a9c63c86b195c4879221dacd
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-12 18:11:37 +0700

    Update mods.py

--- bot/handlers/mods.py
+++ bot/handlers/mods.py
@@ -33,10 +33,10 @@ class SetRankStates(StatesGroup):
 @mods_router.message(Command("set_rank"))
 async def cmd_set_rank(message: Message, state: FSMContext, bot: Bot):
     user_id = message.from_user.id
+    print(user_id, aio_tools.get_chat_owner_id(bot, message.chat.id))
     if not db.has_permission(user_id, 2) or aio_tools.get_chat_owner_id(bot, message.chat.id) == user_id:
         await message.reply("У вас недостаточно прав для выполнения этой команды.")
         return
-    print(user_id, aio_tools.get_chat_owner_id(bot, message.chat.id))
     
     await state.set_state(SetRankStates.waiting_for_username)
     await message.reply("✅ Отлично! Начнём!\n\n✍️ Введите имя пользователя (реплай, юзернейм, айди).")
