342442c1906dd8e8414b7508d80b047f6e487d5a
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-14 23:22:37 +0700

    Исправлено отсутствующее указание роутера

--- bot/__main__.py
+++ bot/__main__.py
@@ -16,6 +16,7 @@ from .handlers.help import help_router
 from .handlers.rp import rp_router
 from .handlers.ai import ai_router
 from .handlers.mods import mods_router
+from .handlers.tag import tag_router
 from .handlers.text import text_router
 
 load_dotenv()
@@ -34,6 +35,7 @@ async def main():
         rp_router,
         ai_router,
         mods_router,
+        tag_router,
         text_router
     )
 
