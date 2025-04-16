3da19b34cc03f89c53dab56046524206564d28e3
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-16 21:36:35 +0700

    Дальше копаемся в форматах

--- bot/handlers/video.py
+++ bot/handlers/video.py
@@ -14,7 +14,7 @@ async def download_video(url: str) -> dict:
     output_path = CACHE_DIR / random_filename
 
     process = await asyncio.create_subprocess_exec(
-        'yt-dlp', '-f', 'bestvideo[height<=540]+bestaudio/best[height<=540]/best[height<=540]', '-o', str(output_path), url,
+        'yt-dlp', '-f', 'worst/worstvideo+worstaudio/best', '-o', str(output_path), url,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
