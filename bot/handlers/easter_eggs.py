6c9666629aac0ec05097c9c2766fbbca6af78da4
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-18 00:19:13 +0700

    Поддержка http.cat

--- bot/handlers/easter_eggs.py
+++ bot/handlers/easter_eggs.py
@@ -1,10 +1,21 @@
+import random
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, URLInputFile
 from aiogram.enums import ParseMode
 from pathlib import Path
 
 easter_router = Router()
+
+# Список HTTP-кодов, для которых доступны изображения на http.cat
+http_codes = [
+    100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 214, 226,
+    300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406,
+    407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 419, 420, 421, 422,
+    423, 424, 425, 426, 428, 429, 431, 444, 450, 451, 495, 496, 497, 498, 499,
+    500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525,
+    530, 599
+]
 
 @easter_router.message(Command("coffee"))
 async def cmd_tea(message: Message):
@@ -14,3 +25,10 @@ async def cmd_tea(message: Message):
         await message.reply_to_message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
     else:
         await message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
+
+@easter_router.message(Command("http_cat"))
+async def cmd_cat(message: Message):
+    code = random.choice(http_codes)
+    url = f"https://http.cat/{code}.jpg"
+    image = URLInputFile(url=url, filename=f"{code}.jpg")
+    await message.reply_photo(photo=image, caption=f"Ваш HTTP кот: {code}")
