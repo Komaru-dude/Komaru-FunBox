58c921399fe7919af796723a53308fe804330f3e
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-18 21:48:19 +0700

    Добавлена /http_dog

--- bot/handlers/easter_eggs.py
+++ bot/handlers/easter_eggs.py
@@ -8,13 +8,22 @@ from pathlib import Path
 easter_router = Router()
 
 # Список HTTP-кодов, для которых доступны изображения на http.cat
-http_codes = [
+cat_http_codes = [
     100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 214, 226,
     300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406,
     407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 419, 420, 421, 422,
     423, 424, 425, 426, 428, 429, 431, 444, 450, 451, 495, 496, 497, 498, 499,
     500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525,
     530, 599
+]
+dog_http_codes = [  # А это для http.dog
+    100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 218, 226,
+    300, 301, 302, 303, 304, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405,
+    406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 419, 420, 421,
+    422, 423, 424, 425, 426, 428, 429, 430, 431, 440, 444, 449, 451, 460, 463,
+    464, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507,
+    508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 527, 529, 530, 561,
+    598, 599, 999
 ]
 
 @easter_router.message(Command("coffee"))
@@ -28,10 +37,17 @@ async def cmd_tea(message: Message):
 
 @easter_router.message(Command("http_cat"))
 async def cmd_http_cat(message: Message):
-    code = random.choice(http_codes)
+    code = random.choice(cat_http_codes)
     url = f"https://http.cat/{code}.jpg"
     image = URLInputFile(url=url, filename=f"{code}.jpg")
     await message.reply_photo(photo=image, caption=f"Ваш HTTP кот: {code}")
+
+@easter_router.message(Command("http_dog"))
+async def cmd_http_dog(message: Message):
+    code = random.choice(dog_http_codes)
+    url = f"https://http.dog/{code}.jpg"
+    image = URLInputFile(url=url, filename=f"{code}.jpg")
+    await message.reply_photo(photo=image, caption=f"Ваша HTTP собака: {code}")
 
 @easter_router.message(Command("cat"))
 async def cmd_cat(message: Message):
