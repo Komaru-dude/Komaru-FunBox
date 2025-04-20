1db661d969279dcb894b78378a76f3cceb30ce4a
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-20 21:30:33 +0700

    Чуть другой промпт, тестовая версия /deepseek

--- bot/handlers/ai.py
+++ bot/handlers/ai.py
@@ -31,7 +31,7 @@ async def cmd_gemini(message: Message, custom_payload: dict = None):
     payload = custom_payload or {
         "model": "gemini-2.0-flash",
         "request": {
-            "messages": [{"role": "user", "content": f"Не используй форматирование, запрос пользователя: {request}"}]
+            "messages": [{"role": "user", "content": f"Не используй markdown/html форматирование, запрос пользователя: {request}"}]
         }
     }
 
@@ -83,6 +83,55 @@ async def cmd_aggemini(message: Message):
     }
 
     await cmd_gemini(message, custom_payload=custom_payload)
+
+@ai_router.message(Command("deepseek"))
+async def cmd_deepseek(message: Message):
+    base_msg = await message.reply("🔄 Обработка...")
+    split_text = message.text.split(maxsplit=1)
+
+    if db.is_user_banned(message.from_user.id):
+        await message.reply("❌ Вы заблокированы, это действие вам запрещено")
+        return
+
+    if len(split_text) < 2 and not message.reply_to_message:
+        await base_msg.edit_text("❌ Пожалуйста, укажите сообщение для нейросети.")
+        return
+
+    if len(split_text) >= 2 and message.reply_to_message:
+        request = f"\"{message.reply_to_message.text}\"\n{split_text[1]}"
+    elif message.reply_to_message:
+        request = message.reply_to_message.text
+    else:
+        request = split_text[1]
+
+    payload = {
+        "model": "deepseek-r1",
+        "request": {
+            "messages": [{"role": "user", "content": f"Не используй markdown/html форматирование, запрос пользователя: {request}"}]
+        }
+    }
+
+    data, error = await make_post_request(payload)
+
+    if error:
+        await base_msg.edit_text(error)
+        return
+
+    if not data.get("choices"):
+        answer = "⚠️ Ошибка: пустой ответ от API"
+    else:
+        answer = data["choices"][0]["message"]["content"]
+
+    raw_answer = f"💭 Запрос: {request}\n\n🧠 Ответ нейросети: {answer}"
+    if len(raw_answer) > 4096:
+        chunks = [raw_answer[i:i + 4096] for i in range(0, len(raw_answer), 4096)]
+    else:
+        chunks = [raw_answer]
+    for idx, chunk in enumerate(chunks):
+        if idx == 0:
+            await base_msg.edit_text(chunk)
+        else:
+            await base_msg.reply(chunk)
 
 @ai_router.message(Command("search"))
 async def cmd_search(message: Message):
