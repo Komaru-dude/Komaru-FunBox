1a38158f348ff58fb5b866d574fd4f11b7ac899a
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-22 18:43:19 +0700

    Меньше копипасты

--- bot/utils/aio_tools.py
+++ bot/utils/aio_tools.py
@@ -1,5 +1,6 @@
 import aiohttp, os
 from aiogram import Bot
+from aiogram.types import Message
 
 ai_url = os.getenv("API_URL")
 
@@ -9,6 +10,64 @@ async def get_chat_owner_id(bot: Bot, chat_id: int):
         if admin.status == 'creator':
             return admin.user.id
     return None
+
+async def get_user(message: Message):
+    text = message.text.strip()
+    error_msg = None
+    first_name = "Пользователь"
+
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user
+        return user.id, user.first_name or first_name, None
+
+    if message.entities:
+        for entity in message.entities:
+            if entity.type == "text_mention":
+                user = entity.user
+                return user.id, user.first_name or first_name, None
+            
+            if entity.type == "mention":
+                username = text[entity.offset:entity.offset+entity.length].lstrip('@')
+                data = await fetch_user_data(username=username)
+                if data: 
+                    return data['user_id'], data.get('first_name', first_name), None
+                break
+
+    if text.isdigit():
+        data = await fetch_user_data(user_id=int(text), chat_id=message.chat.id)
+    else:
+        data = await fetch_user_data(username=text.lstrip('@'), chat_id=message.chat.id)
+
+    if data and data.get('user_id'):
+        return data['user_id'], data.get('first_name', first_name), None
+
+    error_msg = data.get('error') if data else "Не указан пользователь"
+    await message.reply(f"Ошибка: {error_msg}")
+    return None, None, error_msg
+
+async def fetch_user_data(user_id=None, username=None, chat_id=None):
+    try:
+        if user_id:
+            url = f'http://127.0.0.1:8001/username/{chat_id}/{user_id}'
+        elif username:
+            url = f'http://127.0.0.1:8001/user/{username}'
+        else:
+            return {'error': 'Invalid parameters'}
+
+        data = await fetch_json(url)
+        if data.get('user_id'):
+            data['first_name'] = await get_first_name(chat_id, data['user_id'])
+        return data
+
+    except Exception as e:
+        return {'error': f'API Error: {str(e)}'}
+
+async def get_first_name(chat_id, user_id):
+    try:
+        data = await fetch_json(f'http://127.0.0.1:8001/first_name/{chat_id}/{user_id}')
+        return data.get('first_name', 'Пользователь')
+    except:
+        return 'Пользователь'
 
 async def fetch_json(url):
     async with aiohttp.ClientSession() as session:
