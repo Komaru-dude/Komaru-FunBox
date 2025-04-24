3d8f39acc6f0cd0831922d2e3a83ea8f03a61dec
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-25 00:12:58 +0700

    Шакалим сильнее

--- bot/handlers/image.py
+++ bot/handlers/image.py
@@ -84,7 +84,19 @@ async def cmd_jpeg(message: Message, bot: Bot):
         process = await asyncio.create_subprocess_exec(
             "ffmpeg", "-y",
             "-i", str(input_path),
-            "-q:v", "31",
+            "-vf",
+            "scale=140:-1:flags=neighbor,"
+            "scale=1920:-1:flags=neighbor,"
+            "noise=alls=200:allf=t+u,"
+            "curves=r='0/0 0.5/0.9 1/1':"
+            "g='0/0 0.5/0.9 1/1':"
+            "b='0/0 0.5/0.9 1/1',"
+            "format=yuvj420p",
+            "-qscale:v", "1",
+            "-dct", "fastint",
+            "-huffman", "optimal",
+            "-strip",
+            "-optimize", "0",
             str(output_path),
             stdout=asyncio.subprocess.DEVNULL,
             stderr=asyncio.subprocess.DEVNULL
