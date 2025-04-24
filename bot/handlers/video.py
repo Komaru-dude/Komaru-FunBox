0534c6fab06db7202ca2f221cb7728e06be898c5
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-24 22:49:24 +0700

    Снижаем до 5 секунд

--- bot/handlers/video.py
+++ bot/handlers/video.py
@@ -81,7 +81,7 @@ async def cmd_gif(message: Message, bot: Bot):
             "-i", str(input_path),
             "-c:v", "gif",
             "-vf",
-            "fps=24,scale=320:-1,trim=duration=7",
+            "fps=24,scale=320:-1,trim=duration=5",
             "-loop", "0",
             "-gifflags", "-offsetting",
             str(output_path),
