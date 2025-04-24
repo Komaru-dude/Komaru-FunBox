74810f298b61ff4b25e2290c54f3ebc68ca0c30b
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-24 21:36:06 +0700

    Срезаем до 7 секунд

--- bot/handlers/video.py
+++ bot/handlers/video.py
@@ -79,7 +79,7 @@ async def cmd_gif(message: Message, bot: Bot):
             "-i", str(input_path),
             "-vf", 
             f"fps=24,scale=320:-1:flags=lanczos,"
-            f"trim=duration=15,"
+            f"trim=duration=7,"
             "split=2[a][b];[a]palettegen=stats_mode=diff:max_colors=64[p];[b][p]paletteuse",
             "-loop", "0",
             "-gifflags", "-offsetting",
