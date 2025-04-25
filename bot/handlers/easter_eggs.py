538a07794d8519a284c807783cbb52d9092e2dc9
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-26 00:07:43 +0700

    Переписываем пасхальный хендлер под error_report

--- bot/handlers/easter_eggs.py
+++ bot/handlers/easter_eggs.py
@@ -1,12 +1,13 @@
 import random
-from aiogram import Router
+import traceback
+from aiogram import Router, Bot
 from aiogram.filters import Command
 from aiogram.types import Message, URLInputFile
 from aiogram.enums import ParseMode
+from bot.utils.aio_tools import error_report
 
 easter_router = Router()
 
-# Список HTTP-кодов, для которых доступны изображения на http.cat
 cat_http_codes = [
     100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 214, 226,
     300, 301, 302, 303, 304, 305, 307, 308, 400, 401, 402, 403, 404, 405, 406,
@@ -15,7 +16,7 @@ cat_http_codes = [
     500, 501, 502, 503, 504, 506, 507, 508, 509, 510, 511, 521, 522, 523, 525,
     530, 599
 ]
-dog_http_codes = [  # А это для http.dog
+dog_http_codes = [
     100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 218, 226,
     300, 301, 302, 303, 305, 306, 307, 308, 400, 401, 402, 403, 404, 405,
     406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 419, 420, 421,
@@ -26,63 +27,71 @@ dog_http_codes = [  # А это для http.dog
 ]
 
 @easter_router.message(Command("coffee"))
-async def cmd_tea(message: Message):
-    t418 = URLInputFile(url="https://http.cat/418.jpg", filename="418.jpg")
-    if message.reply_to_message:
-        await message.reply_to_message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
-    else:
-        await message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
+async def cmd_tea(message: Message, bot: Bot):
+    try:
+        t418 = URLInputFile(url="https://http.cat/418.jpg", filename="418.jpg")
+        if message.reply_to_message:
+            await message.reply_to_message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
+        else:
+            await message.reply_photo(t418, caption="418 I'm a <a href='https://ru.wikipedia.org/wiki/HTCPCP'>teapot</a> ☕", parse_mode=ParseMode.HTML)
+    except Exception:
+        await error_report(message, bot, "coffee", traceback.format_exc())
 
 @easter_router.message(Command("http_cat"))
-async def cmd_http_cat(message: Message):
-    split_text = message.text.split()
-    code = None
+async def cmd_http_cat(message: Message, bot: Bot):
+    try:
+        split_text = message.text.split()
+        code = None
 
-    if len(split_text) > 1:
-        try:
-            user_code = int(split_text[1])
-            if user_code == 418:
-                code = 405
-            elif user_code in cat_http_codes:
-                code = user_code
-        except ValueError:
-            pass  # Введено что-то нечисловое
+        if len(split_text) > 1:
+            try:
+                user_code = int(split_text[1])
+                code = 405 if user_code == 418 else user_code if user_code in cat_http_codes else None
+            except ValueError:
+                pass
 
-    if code is None:
-        code = random.choice(cat_http_codes)
-
-    url = f"https://http.cat/{code}.jpg"
-    image = URLInputFile(url=url, filename=f"{code}.jpg")
-    await message.reply_photo(photo=image, caption=f"Ваш HTTP кот: {code}")
+        code = code or random.choice(cat_http_codes)
+        await message.reply_photo(
+            photo=URLInputFile(f"https://http.cat/{code}.jpg", f"{code}.jpg"),
+            caption=f"Ваш HTTP кот: {code}"
+        )
+    except Exception:
+        await error_report(message, bot, "http_cat", traceback.format_exc())
 
 @easter_router.message(Command("http_dog"))
-async def cmd_http_dog(message: Message):
-    split_text = message.text.split()
-    code = None
+async def cmd_http_dog(message: Message, bot: Bot):
+    try:
+        split_text = message.text.split()
+        code = None
 
-    if len(split_text) > 1:
-        try:
-            user_code = int(split_text[1])
-            if user_code == 418:
-                code = 405
-            elif user_code in dog_http_codes:
-                code = user_code
-        except ValueError:
-            pass
+        if len(split_text) > 1:
+            try:
+                user_code = int(split_text[1])
+                code = 405 if user_code == 418 else user_code if user_code in dog_http_codes else None
+            except ValueError:
+                pass
 
-    if code is None:
-        code = random.choice(dog_http_codes)
-
-    url = f"https://http.dog/{code}.jpg"
-    image = URLInputFile(url=url, filename=f"{code}.jpg")
-    await message.reply_photo(photo=image, caption=f"Ваша HTTP собака: {code}")
+        code = code or random.choice(dog_http_codes)
+        await message.reply_photo(
+            photo=URLInputFile(f"https://http.dog/{code}.jpg", f"{code}.jpg"),
+            caption=f"Ваша HTTP собака: {code}"
+        )
+    except Exception:
+        await error_report(message, bot, "http_dog", traceback.format_exc())
 
 @easter_router.message(Command("cat"))
-async def cmd_cat(message: Message):
-    cat = URLInputFile("https://cataas.com/cat")
-    await message.reply_photo(cat, caption="🐈‍⬛ Ваш кот:")
+async def cmd_cat(message: Message, bot: Bot):
+    try:
+        await message.reply_photo(
+            URLInputFile("https://cataas.com/cat"),
+            caption="🐈‍⬛ Ваш кот:"
+        )
+    except Exception:
+        await error_report(message, bot, "cat", traceback.format_exc())
 
 @easter_router.message(Command("cat_gif"))
-async def cmd_cat_gif(message: Message):
-    cat = URLInputFile("https://cataas.com/cat/gif")
-    await message.reply_video(cat)
+async def cmd_cat_gif(message: Message, bot: Bot):
+    try:
+        await message.reply_video(URLInputFile("https://cataas.com/cat/gif"))
+    except Exception:
+        await error_report(message, bot, "cat_gif", traceback.format_exc())
