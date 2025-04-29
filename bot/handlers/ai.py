d8390ae8a629988709139d13ad4be363c98adb04
Komaru-dude <honkhonkovic842@gmail.com>
2025-04-29 19:25:06 +0700

    Меняем промпт

--- bot/handlers/ai.py
+++ bot/handlers/ai.py
@@ -371,7 +371,7 @@ async def cmd_vocr(message: Message, bot: Bot):
 
         vocr_url = "https://api.jigsawstack.com/v1/vocr"
         payload = {
-            "prompt": ["first name", "last name"],
+            "prompt": ["thing"],
             "file_store_key": file_store_key,
         }
         headers = {"x-api-key": jigsaw_api_key}
