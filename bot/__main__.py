d9ccff29512070f188017264ccbb1222e953e48e
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-02 20:14:06 +0700

    Исправляем циклический импорт

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 import os
 import subprocess
@@ -10,6 +9,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.methods import DeleteWebhook
 from bot.utils.aio_tools import fetch_json
+from bot.utils.global_storage import known_models
 
 from .handlers.basic import base_router
 from .handlers.etc import etc_router
@@ -30,7 +30,6 @@ logging.basicConfig(level=logging.INFO)
 token = os.getenv("BOT_API_TOKEN")
 bot = Bot(token)
 dp = Dispatcher()
-known_models = set()
 
 
 async def fetch_models():
@@ -71,7 +70,7 @@ def clear_cache():
 
 async def main():
     clear_cache()
-    fetch_models()
+    await fetch_models()
 
     dp.include_routers(
         base_router,
@@ -102,9 +101,7 @@ async def main():
             "--port",
             "8001",
         ],
-        creationflags=(
-            subprocess.CREATE_NEW_PROCESS_GROUP if sys.platform == "win32" else 0
-        ),
+        creationflags=(subprocess.CREATE_NEW_PROCESS_GROUP if sys.platform == "win32" else 0),
     )
 
     try:
@@ -122,11 +119,3 @@ async def main():
             except subprocess.TimeoutExpired:
                 pyrogram_process.kill()
 
-
-if __name__ == "__main__":
-    if sys.platform == "win32":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Основной процесс завершён.")
--- bot/utils/global_storage.py
+++ bot/utils/global_storage.py
@@ -2,3 +2,5 @@ import asyncio
 
 argue_active_chats = []
 argue_active_chats_lock = asyncio.Lock()
+
+known_models = set()
