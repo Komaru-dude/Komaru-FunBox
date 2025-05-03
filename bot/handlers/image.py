e040302bad1bbdd474bf0c55ba09cc3bd2514eed
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-03 17:53:23 +0700

    Удаляем остатки /lick

--- bot/handlers/image.py
+++ bot/handlers/image.py
@@ -22,47 +22,6 @@ async def get_last_profile_photo(user_id, bot):
     return last_photo_set[-1]
 
 
-# Сделать нормальную реализацию позже
-# @image_router.message(Command("lick"))
-async def cmd_lick(message: Message, bot: Bot):
-    user_id = (
-        message.reply_to_message.from_user.id
-        if message.reply_to_message
-        else message.from_user.id
-    )
-
-    if not (profile_photo := await get_last_profile_photo(user_id, bot)):
-        await message.reply("❌ У пользователя нет фото профиля!")
-        return
-
-    # Пути к файлам
-    media_dir = Path(__file__).resolve().parent.parent / "media"
-    template_path = media_dir / "lickbg.jpg"
-
-    try:
-        file = await bot.get_file(profile_photo.file_id)
-        user_photo_path = CACHE_DIR / f"user_{user_id}_photo.jpg"
-        await bot.download_file(file.file_path, destination=user_photo_path)
-
-        # Обрабатываем изображение
-        output_path = CACHE_DIR / f"lick_result_{user_id}.jpg"
-        await asyncio.to_thread(
-            replace_green_screen,
-            template_path=str(template_path),
-            new_bg_path=str(user_photo_path),
-            output_path=str(output_path),
-        )
-
-        # Отправляем результат
-        await message.answer_photo(FSInputFile(output_path))
-
-    except Exception:
-        await error_report(message, bot, "lick", traceback.format_exc())
-    finally:
-        user_photo_path.unlink(missing_ok=True)
-        output_path.unlink(missing_ok=True)
-
-
 @image_router.message(Command("jpeg"))
 async def cmd_jpeg(message: Message, bot: Bot):
     command = "jpeg"
