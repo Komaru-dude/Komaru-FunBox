9ad728363abfe18376e19b53bf5f7fb96d4b092b
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-03 18:15:22 +0700

    Правильно выдаём права при размуте

--- bot/handlers/mods.py
+++ bot/handlers/mods.py
@@ -449,13 +449,23 @@ async def cmd_unmute(message: Message, bot: Bot):
         if not target_user_id:
             await message.reply(f"❌ {error_msg}")
             return
+        
+        chat_info = await bot.get_chat(chat_id)
+        permissions = chat_info.permissions or ChatPermissions(
+                can_send_messages=True,
+                can_send_audios=True,
+                can_send_other_messages=True,
+                can_send_documents=True,
+                can_send_photos=True,
+                can_send_polls=True,
+                can_send_video_notes=True,
+                can_send_videos=True,
+            ),
 
         await bot.restrict_chat_member(
             chat_id=chat_id,
             user_id=target_user_id,
-            permissions=ChatPermissions(
-                can_send_messages=True,
-            ),
+            permissions=permissions
         )
         await message.reply(
             f"✅ Пользователь <b>{target_user_id}</b> размьючен",
