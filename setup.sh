b2f429619489e67449f03f89424fd162cf6d327c
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-05 00:03:25 +0700

    Используем apt вместо apt-get

--- setup.sh
+++ setup.sh
@@ -25,8 +25,8 @@ fi
 echo "🚀 Starting Komaru FunBox installation..."
 
 echo "🔄 Updating packages and installing dependencies..."
-apt-get update
-apt-get install -y python3-venv git build-essential autoconf automake libtool pkg-config yt-dlp ffmpeg postgresql
+apt update
+apt install -y python3-venv git build-essential autoconf automake libtool pkg-config yt-dlp ffmpeg postgresql
 snap install gifski
 
 if ! id -u ${USER_NAME} >/dev/null 2>&1; then
