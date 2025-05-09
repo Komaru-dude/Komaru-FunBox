c626c599127d858d52b9993a04f31f52909e1a45
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-09 19:44:06 +0700

    Правим UnboundLocalError

--- bot/utils/aio_tools.py
+++ bot/utils/aio_tools.py
@@ -5,7 +5,7 @@ import os
 import logging
 from aiogram import Bot
 from aiogram.types import Message
-from bot.utils.global_storage import error_report_lock
+from bot.utils.global_storage import error_report_lock, error_report_timestamps
 
 API_HOST = "http://127.0.0.1:8001"
 
