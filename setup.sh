1c3ff128519c3690a8f9fb22ed698101674d1554
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-09 19:29:45 +0700

    Даём юзеру бота просматривать systemd журналы

--- setup.sh
+++ setup.sh
@@ -149,6 +149,7 @@ chown -R ${USER_NAME}:${GROUP_NAME} ${INSTALL_DIR}
 chmod 700 ${INSTALL_DIR}
 chmod +x ${INSTALL_DIR}/force-pull.sh
 echo "komaru ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart komaru-funbox.service" | visudo -f /etc/sudoers.d/komaru-funbox
+usermod -aG systemd-journal ${USER_NAME}
 chmod 600 db_credentials.txt
 
 echo "🔄 Reloading systemd and enabling service..."
