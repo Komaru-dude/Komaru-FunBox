0042105da09471e43630505f391a1e97f30fde7c
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-13 21:24:58 +0700

    Initial commit

---
+++ funbox-wiki/docusaurus.config.js
@@ -0,0 +1,109 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Komaru FunBox wiki',
+  tagline: 'Panchan is cool',
+  favicon: 'img/logo.jpg',
+
+  url: 'https://komaru-dude.github.io',
+  baseUrl: '/Komaru-FunBox/', 
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+      navbar: {
+        title: 'Komaru FunBox wiki',
+        logo: {
+          alt: 'FunBox Logo',
+          src: 'img/logo.jpg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'fstartSidebar',
+            position: 'left',
+            label: 'Быстрый старт',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Документация',
+            items: [
+              {
+                label: 'Быстрый старт',
+                to: '/docs/faststart',
+              },
+            ],
+          },
+          {
+            title: 'Комьюнити',
+            items: [
+              {
+                label: 'Телеграм владельца',
+                href: 'https://t.me/komaru_dude',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/Komaru-dude/Komaru-FunBox',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Komaru-FunBox. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
