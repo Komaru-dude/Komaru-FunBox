484056a90a9611dcd3f5135a229b7c7e4c50c11f
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-14 01:16:07 +0700

    Убираем лишние комментарии, заполняем поля

--- funbox-wiki/docusaurus.config.js
+++ funbox-wiki/docusaurus.config.js
@@ -1,12 +1,4 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -16,18 +8,12 @@ const config = {
 
   url: 'https://komaru-dude.github.io',
   baseUrl: '/Komaru-FunBox/', 
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Komaru-dude',
+  projectName: 'Komaru-FunBox',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru'],
