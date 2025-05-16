78a565e165ae4e3c0935e7eba1b6c9b7d2090f82
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-17 00:59:16 +0700

    bot: handlers: basic: пишем новостной канал в /start

--- bot/handlers/basic.py
+++ bot/handlers/basic.py
@@ -35,6 +35,7 @@ async def cmd_start(message: Message):
         "❔ Если хочешь узнать более подробную информацию о командах, напиши /help.\n\n"
         "👤 Владелец бота: @komaru_dude\n"
         "📚 Гайд по настройке бота: https://komaru-dude.github.io/Komaru-FunBox/docs/setup/faststart\n"
+        "📰 Новостной канал бота: @komaru_funbox\n"
         "🧑‍💻 Исходный код бота: https://github.com/Komaru-dude/Komaru-FunBox\n\n"
         "🎩 Приятного использования!",
         disable_web_page_preview=True,
