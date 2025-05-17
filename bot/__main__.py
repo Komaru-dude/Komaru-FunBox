4e75a0dbc0ff883a7b242a6b5467196c2b3abf85
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-18 01:26:07 +0700

    Правильное имя мидлвари

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,7 +12,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.methods import DeleteWebhook
 
 from bot.database import BASE_DIR, Database
-from bot.middlewares import chatadd
+from bot.middlewares.chatadd import ChatAddMiddleware
 from bot.utils.global_storage import onlysq_models
 from bot.utils.aio_tools import fetch_json
 
@@ -35,7 +35,7 @@ logging.basicConfig(level=logging.INFO)
 token = os.getenv("BOT_API_TOKEN")
 bot = Bot(token)
 dp = Dispatcher()
-dp.message.outer_middleware(chatadd())
+dp.message.outer_middleware(ChatAddMiddleware())
 db = Database()
 DATA_DIR = BASE_DIR / "data"
 
