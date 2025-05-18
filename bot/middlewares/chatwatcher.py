a128cf07729a76e4c2a2253012d8c3117b897c78
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-18 22:48:52 +0700

    Продвинутая обработка чатов

--- bot/middlewares/chatwatcher.py
+++ bot/middlewares/chatwatcher.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import traceback
 from typing import Callable, Dict, Any, Awaitable
 from aiogram import Bot, BaseMiddleware
 from aiogram.types import Message, CallbackQuery, TelegramObject
@@ -15,27 +16,50 @@ class ChatWatcher(BaseMiddleware):
         event: TelegramObject,
         data: Dict[str, Any],
     ) -> Any:
-        event_type = data.get("event_update_type")
-        bot: Bot = data["bot"]
-        user_id = None
-        chat_id = None
+        try:
+            event_type = data.get("event_update_type")
+            bot: Bot = data["bot"]
+            user_id = None
+            chat_id = None
 
-        if isinstance(event, Message):
-            user_id = event.from_user.id
-            chat_id = event.chat.id
-            chat_name = event.chat.full_name
-        elif isinstance(event, CallbackQuery):
-            user_id = event.from_user.id
-            if event.message:
-                chat_id = event.message.chat.id
-                chat_name = event.message.chat.full_name
+            if isinstance(event, Message):
+                user_id = event.from_user.id
+                chat_id = event.chat.id
+                chat_name = event.chat.full_name
+                chat_type = event.chat.type
+            elif isinstance(event, CallbackQuery):
+                return await handler(event, data)
 
-        is_chat_init = await db.chat_exists(chat_id)
+            is_chat_init = await db.chat_exists(chat_id)
 
-        if not is_chat_init:
-            await db.add_chat(chat_id, chat_data={"type": "private"})
-            logging.info(f"Новый чат: {chat_id}, имя: {chat_name}")
-            await bot.send_message(
-                os.getenv("OWNER_ID"), f"🔔 Новый чат: {chat_id}, имя: {chat_name}"
+            if not is_chat_init:
+                await db.add_chat(chat_id, chat_data={"type": chat_type})
+                if chat_type == "private":
+                    logging.info(
+                        f"Новый пользователь бота: {chat_id}, имя: {chat_name}"
+                    )
+                    await bot.send_message(
+                        os.getenv("OWNER_ID"),
+                        f"🔔 Новый пользователь бота: {chat_id}, имя: {chat_name}",
+                    )
+                else:
+                    logging.info(f"Новый чат: {chat_id}, имя: {chat_name}")
+                    await bot.send_message(
+                        os.getenv("OWNER_ID"),
+                        f"🔔 Новый чат: {chat_id}, имя: {chat_name}",
+                    )
+            return await handler(event, data)
+        except Exception:
+            logging.error(
+                f"❌ Не удалось проверить чат.\n\n📛 Traceback: {traceback.format_exc()}"
             )
-        return await handler(event, data)
+            try:
+                await bot.send_message(
+                    os.getenv("OWNER_ID"),
+                    f"❌ Не удалось проверить чат.\n\n📛 Traceback: {traceback.format_exc()}",
+                )
+            except Exception:
+                logging.error(
+                    f"❌ Не удалось отправить овнеру репорт, ошибка: {traceback.format_exc()}"
+                )
+            return await handler(event, data)
