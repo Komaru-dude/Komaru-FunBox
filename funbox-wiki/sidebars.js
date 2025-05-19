0fe56053d7e0617e916a51b327cdd7e40d02058e
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-19 12:00:27 +0700

    /cancel

--- funbox-wiki/sidebars.js
+++ funbox-wiki/sidebars.js
@@ -33,7 +33,7 @@ const sidebars = {
         type: 'doc', 
         id: 'commands/commands'
       },
-      items: ['commands/start', 'commands/status', 'commands/update', 'commands/restart', 'commands/logs', 'commands/help'],
+      items: ['commands/start', 'commands/status', 'commands/update', 'commands/restart', 'commands/logs', 'commands/help', 'commands/cancel'],
     },
   ],
 
