d8adaee8d2b0076e70931363b4d2b03ad4deda16
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-20 00:42:18 +0700

    Update chatwatcher.py

--- bot/middlewares/chatwatcher.py
+++ bot/middlewares/chatwatcher.py
@@ -18,7 +18,7 @@ class ChatWatcher(BaseMiddleware):
     ) -> Any:
         try:
             bot: Bot = data["bot"]
-            bot_username = (await bot.me()).username
+            bot_username = (await bot.me()).username.lower()
 
             if isinstance(event, CallbackQuery):
                 return await handler(event, data)
@@ -27,59 +27,57 @@ class ChatWatcher(BaseMiddleware):
                 user = event.from_user
                 chat = event.chat
                 text = event.text or ""
+                chat_type = chat.type
+                is_bot_command = False
+
+                if text and event.entities:
+                    for entity in event.entities:
+                        if entity.type == "bot_command":
+                            command = text[
+                                entity.offset : entity.offset + entity.length
+                            ].lower()
+                            if "@" in command:
+                                cmd, mentioned_bot = command.split("@", 1)
+                                if mentioned_bot == bot_username:
+                                    is_bot_command = True
+                                    break
+                            else:
+                                if chat_type == "private":
+                                    is_bot_command = True
+                                    break
+                                else:
+                                    is_bot_command = True
+                                    break
+
                 user_id = user.id
                 chat_id = chat.id
-                chat_type = chat.type
                 chat_name = chat.full_name
                 user_name = user.full_name
                 language_code = user.language_code
-
-                if not text:
-                    return await handler(event, data)
 
                 if not await db.chat_exists(chat_id):
                     await db.add_chat(chat_id, chat_data={"type": chat_type})
                     if chat_type != "private":
                         msg = f"🔔 Новый чат: {chat_id}, имя: {chat_name}"
                         logging.info(msg)
-                        owner_id = os.getenv("OWNER_ID")
-                        if owner_id:
+                        if owner_id := os.getenv("OWNER_ID"):
                             await bot.send_message(owner_id, msg)
 
-                if event.entities:
-                    for entity in event.entities:
-                        if entity.type == "bot_command":
-                            command = text[
-                                entity.offset : entity.offset + entity.length
-                            ]
-                            if "@" in command:
-                                mentioned_bot = command.split("@")[1].lower()
-                                if mentioned_bot != bot_username.lower():
-                                    return await handler(event, data)
-
-                if chat_type == "private" or text.startswith("/"):
-                    user_info = await db.get_global_user(user_id)
-                    if user_info is None:
+                if chat_type == "private" or is_bot_command:
+                    if not await db.get_global_user(user_id):
                         await db.add_global_user(
                             user_id, {"language_code": language_code}
                         )
                         msg = f"🔔 Новый пользователь бота: {user_id}, имя: {user_name}"
                         logging.info(msg)
-                        owner_id = os.getenv("OWNER_ID")
-                        if owner_id:
+                        if owner_id := os.getenv("OWNER_ID"):
                             await bot.send_message(owner_id, msg)
 
             return await handler(event, data)
         except Exception:
-            logging.error("❌ Не удалось проверить чат.", exc_info=True)
-            try:
-                owner_id = os.getenv("OWNER_ID")
-                if owner_id:
-                    await bot.send_message(
-                        owner_id,
-                        f"❌ Не удалось проверить чат.\n\n📛 Traceback: {traceback.format_exc()}",
-                    )
-            except Exception:
-                logging.error("❌ Не удалось отправить овнеру репорт.", exc_info=True)
-
+            logging.error("❌ Ошибка в ChatWatcher:", exc_info=True)
+            if owner_id := os.getenv("OWNER_ID"):
+                await bot.send_message(
+                    owner_id, f"❌ Ошибка в ChatWatcher:\n\n{traceback.format_exc()}"
+                )
             return await handler(event, data)
