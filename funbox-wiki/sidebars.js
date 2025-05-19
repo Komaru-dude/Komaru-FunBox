235c2c8a86e6233e36f4ae92d32829d93cbfc492
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-19 12:03:17 +0700

    /new_year

--- funbox-wiki/sidebars.js
+++ funbox-wiki/sidebars.js
@@ -33,7 +33,7 @@ const sidebars = {
         type: 'doc', 
         id: 'commands/commands'
       },
-      items: ['commands/start', 'commands/status', 'commands/update', 'commands/restart', 'commands/logs', 'commands/help', 'commands/cancel'],
+      items: ['commands/start', 'commands/status', 'commands/update', 'commands/restart', 'commands/logs', 'commands/help', 'commands/cancel', 'commands/new_year'],
     },
   ],
 
