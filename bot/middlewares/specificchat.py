af6a11613626eda28ad4b659de0375b0848cbecf
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-23 14:42:04 +0700

    Поддержка работы только в спеицифичных чатах

---
+++ bot/middlewares/specificchat.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import traceback
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import Bot, BaseMiddleware
+from aiogram.types import Message, CallbackQuery, TelegramObject
+from bot import database
+
+db = database.Database()
+
+class SpecificChat(BaseMiddleware):
+    def __init__(self):
+        super().__init__()
+        self.restrict_enabled = os.getenv("SPECIFIC_CHAT", "").strip().lower() == "true"
+        specific_chats = os.getenv("SPECIFIC_CHATS_ID", "")
+        self.allowed_chat_ids = [int(cid.strip()) for cid in specific_chats.split(",") if cid.strip().isdigit()]
+        self.owner_id = os.getenv("OWNER_ID")
+
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        try:
+            bot: Bot = data["bot"]
+
+            if isinstance(event, CallbackQuery):
+                return await handler(event, data)
+
+            if isinstance(event, Message):
+                chat_id = event.chat.id
+                if self.restrict_enabled and self.allowed_chat_ids:
+                    if chat_id not in self.allowed_chat_ids:
+                        return None
+                return await handler(event, data)
+
+        except Exception:
+            logging.error("❌ Ошибка в SpecificChat:", exc_info=True)
+            if self.owner_id:
+                await bot.send_message(
+                    self.owner_id, f"❌ Ошибка в SpecificChat:\n\n{traceback.format_exc()}"
+                )
+            return await handler(event, data)
