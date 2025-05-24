73495fdfca0b8866fe392036a52c4bb763982911
Komaru-dude <honkhonkovic842@gmail.com>
2025-05-24 11:12:50 +0700

    Тест фильтров

--- bot/handlers/economy.py
+++ bot/handlers/economy.py
@@ -1,7 +1,14 @@
 from aiogram import Router, Bot
 from aiogram.types import Message
+from aiogram.filters import Command
 from bot import database
+from bot.filters import func_filter, cooldown_filter
 from bot.utils.aio_tools import error_report
 
 eco_router = Router()
 db = database.Database()
+
+
+@eco_router.message(Command("work"), func_filter(func_name="eco"), cooldown_filter(command="work", cooldown=14400))
+async def cmd_work(message: Message, bot: Bot):
+    await message.reply("1")
