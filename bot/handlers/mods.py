cb9a10436cd08f740fddab6ca0be51ec5d44b2cb
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-11 00:01:30 +0700

    bot: handlers: mods: Use correct money number

--- bot/handlers/mods.py
+++ bot/handlers/mods.py
@@ -310,7 +310,7 @@ async def cmd_info(message: Message, bot: Bot):
             f"🔇 Мьюты: {user_data['mutes']}\n"
             f"🔨 Баны: {user_data['bans']}\n"
             f"💎 Репутация: {user_data['reputation']}\n"
-            f"{chat_data["currency_sign"]} Монет: {user_data["money"]}\n"
+            f"🪙 Монет: {await db.get_global_user_param(user_info['user_id'], "money")}\n"
             f"📨 Сообщений: {user_data['message_count']}\n"
             f"🏅 Ранг: {user_data['rank']}\n"
         )
