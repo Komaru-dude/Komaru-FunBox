a7b477fd509a48bf337b9c102b829cac8df3027f
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-12 22:51:56 +0700

    bot: handlers: economy: Switch to economy table

--- bot/handlers/economy.py
+++ bot/handlers/economy.py
@@ -22,18 +22,17 @@ eco_router = Router()
 async def cmd_work(message: Message, bot: Bot, db: Database):
     try:
         user_id = message.from_user.id
-        chat_id = message.chat.id
-        chat_data = await db.get_chat(chat_id)
+        eco_data = await db.get_eco_settings()
         current_bal = await db.get_global_user_param(user_id, "money")
 
-        min_income = chat_data["min_work_income"]
-        max_income = chat_data["max_work_income"]
+        min_income = eco_data["min_work_income"]
+        max_income = eco_data["max_work_income"]
         current_income = random.randint(min_income, max_income)
 
         new_bal = current_bal + current_income
         await db.set_global_user_param(user_id, "money", new_bal)
         await message.reply(
-            f"👨‍💻 Вы заработали: {current_income}\n{chat_data["currency_sign"]} Ваш новый баланс: {new_bal}"
+            f"👨‍💻 Вы заработали: {current_income}\n{eco_data["currency_sign"]} Ваш новый баланс: {new_bal}"
         )
     except Exception:
         await db.reset_cooldown(user_id, "work")
@@ -48,34 +47,33 @@ async def cmd_work(message: Message, bot: Bot, db: Database):
 async def cmd_steal(message: Message, bot: Bot, db: Database):
     try:
         user_id = message.from_user.id
-        chat_id = message.chat.id
-        chat_data = await db.get_chat(chat_id)
+        eco_data = await db.get_eco_settings()
         current_bal = await db.get_global_user_param(user_id, "money")
-        if current_bal < chat_data["max_steal_penalty"] / 2:
+        if current_bal < eco_data["max_steal_penalty"] / 2:
             await message.reply(
-                f"❌ Вам нужно иметь на балансе хотя бы половину от максимальной суммы штрафа ({chat_data["currency_sign"]}{chat_data['max_steal_penalty'] / 2})"
+                f"❌ Вам нужно иметь на балансе хотя бы половину от максимальной суммы штрафа ({eco_data["currency_sign"]}{eco_data['max_steal_penalty'] / 2})"
             )
             await db.reset_cooldown(user_id, "steal")
             return
 
-        min_income = chat_data["min_steal_income"]
-        max_income = chat_data["max_steal_income"]
+        min_income = eco_data["min_steal_income"]
+        max_income = eco_data["max_steal_income"]
         current_income = random.randint(min_income, max_income)
 
-        min_penalty = chat_data["min_steal_penalty"]
-        max_penalty = chat_data["max_steal_penalty"]
+        min_penalty = eco_data["min_steal_penalty"]
+        max_penalty = eco_data["max_steal_penalty"]
         current_penalty = random.randint(min_penalty, max_penalty)
 
-        fail_percent = chat_data["steal_fail_percent"]
+        fail_percent = eco_data["steal_fail_percent"]
         if random.randint(1, 100) <= fail_percent:
             new_bal = current_bal - current_penalty
             await message.reply(
-                f"😔 Вам не повезло.\n🧨 Вы потеряли: {current_penalty}\n{chat_data["currency_sign"]} Ваш новый баланс: {new_bal}"
+                f"😔 Вам не повезло.\n🧨 Вы потеряли: {current_penalty}\n{eco_data["currency_sign"]} Ваш новый баланс: {new_bal}"
             )
         else:
             new_bal = current_bal + current_income
             await message.reply(
-                f"🤑 Повезло!\n💡 Вы заработали: {current_income}\n{chat_data["currency_sign"]} Ваш новый баланс: {new_bal}"
+                f"🤑 Повезло!\n💡 Вы заработали: {current_income}\n{eco_data["currency_sign"]} Ваш новый баланс: {new_bal}"
             )
 
         await db.set_global_user_param(user_id, "money", new_bal)
@@ -93,9 +91,8 @@ async def cmd_steal(message: Message, bot: Bot, db: Database):
 async def cmd_rob(message: Message, bot: Bot, db: Database):
     try:
         user_id = message.from_user.id
-        chat_id = message.chat.id
         split_text = message.text.split()
-        chat_data = await db.get_chat(chat_id)
+        eco_data = await db.get_eco_settings()
         target_id, get_id_error = await get_user_id(message)
 
         if len(split_text) < 2 and not message.reply_to_message:
@@ -119,14 +116,14 @@ async def cmd_rob(message: Message, bot: Bot, db: Database):
             return
 
         succeed_percent = random.randint(
-            chat_data["rob_min_percent"], chat_data["rob_max_percent"]
+            eco_data["rob_min_percent"], eco_data["rob_max_percent"]
         )
         if target_user_bal * succeed_percent / 100 < 1:
             await message.reply("❌ У цели недостаточно наличных")
             await db.reset_cooldown(user_id, "rob")
             return
 
-        fail_percent = chat_data["rob_fail_percent"]
+        fail_percent = eco_data["rob_fail_percent"]
         if random.randint(1, 100) <= fail_percent:
             new_bal = user_bal / 2
             await message.reply(f"😔 Вам не повезло.\n🧨 Ваш новый баланс: {new_bal}")
@@ -135,7 +132,7 @@ async def cmd_rob(message: Message, bot: Bot, db: Database):
             target_new_bal = target_user_bal - target_penalty
             new_bal = user_bal + target_penalty
             await message.reply(
-                f"🤑 Повезло!\n💡 Вы украли: {target_penalty}\n{chat_data["currency_sign"]}\nНовый баланс цели {target_new_bal}\nВаш новый баланс: {new_bal}"
+                f"🤑 Повезло!\n💡 Вы украли: {target_penalty}\n{eco_data["currency_sign"]}\nНовый баланс цели {target_new_bal}\nВаш новый баланс: {new_bal}"
             )
 
         await db.set_global_user_param(user_id, "money", new_bal)
@@ -144,7 +141,7 @@ async def cmd_rob(message: Message, bot: Bot, db: Database):
     except ZeroDivisionError:
         profile_link = f"tg://user?id={os.getenv('OWNER_ID')}"
         await message.reply(
-            f'❌ Произошло деление на ноль! Убедитесь что все используемые командой значения больше нуля.\nЕсли всё корректно, обратитесь к владельцу: <a href="{profile_link}">Тык</a>',
+            f'❌ Произошло деление на ноль! Обратитесь к владельцу: <a href="{profile_link}">Тык</a>',
             parse_mode=ParseMode.HTML,
         )  # Не используем юзернейм во избежании его изменения
     except Exception:
