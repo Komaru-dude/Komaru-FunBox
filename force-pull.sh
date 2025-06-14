6e76bff7669e5ef52b8cb4d2de057e96ec5854ad
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-14 14:25:20 +0700

    Update force-pull.sh

--- force-pull.sh
+++ force-pull.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 cd "$1" || exit 1
 
-# Выбираем ветку в зависимости от наличия файла 'test'
-if [ -f test ]; then
-    BRANCH="test"
-else
-    BRANCH="release"
-fi
-
-# Переключаемся на нужную ветку
-git checkout "$BRANCH" || exit 1
-
 # Обновляем локальные ссылки
 git fetch origin
 
 # Пытаемся обновить ветку с rebase
-if ! git pull --rebase origin "$BRANCH"; then
+if ! git pull --rebase; then
     echo "⚠ git pull failed, doing hard reset..."
-    git reset --hard "origin/$BRANCH"
+    git reset --hard
     git clean -df
 fi
