041b0fae1f489a7f559d0bf4f00aa95096b1942b
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-16 12:42:54 +0700

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -3,7 +3,7 @@ import sys
 
 def setup_logger():
     formatter = logging.Formatter(
-        '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+        '%(asctime)s %(levelname)s %(name)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
