b5bb47087949d94834fe91e38e41febd6ed7cf6d
Komaru-dude <honkhonkovic842@gmail.com>
2025-06-20 22:38:27 +0700

    code style: black

--- bot/handlers/economy.py
+++ bot/handlers/economy.py
@@ -221,9 +221,13 @@ async def bet_chosen(message: Message, bot: Bot, db: Database, state: FSMContext
         await state.set_state(Dice.choose_dice)
 
     except ValueError:
-        await message.reply("❌ Это не число. Пожалуйста, отправьте число или /cancel для остановки")
+        await message.reply(
+            "❌ Это не число. Пожалуйста, отправьте число или /cancel для остановки"
+        )
     except TypeError:
-        await message.reply("❌ Это не число. Пожалуйста, отправьте число или /cancel для остановки")
+        await message.reply(
+            "❌ Это не число. Пожалуйста, отправьте число или /cancel для остановки"
+        )
     except Exception:
         await error_report(message, bot, "bet_chosen", traceback.format_exc())
 
