79d4ffe78dadca64c1ede09ce1ef4d8f66dca1dd
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-09 21:27:59 +0700

    style(дуэли): black

--- bot/handlers/economy.py
+++ bot/handlers/economy.py
@@ -566,7 +566,11 @@ class Duel(StatesGroup):
     choose_bet = State()
 
 
-@eco_router.message(Command("duel"), CooldownFilter("duel", 30), ChatTypeFilter(chat_type=["group", "supergroup"]))
+@eco_router.message(
+    Command("duel"),
+    CooldownFilter("duel", 30),
+    ChatTypeFilter(chat_type=["group", "supergroup"]),
+)
 async def cmd_duel(message: Message, bot: Bot, state: FSMContext, db: Database):
     try:
         chat_id = message.chat.id
