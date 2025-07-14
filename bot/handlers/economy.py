388236150efb62e212d9df51f1f33ff51525aa1d
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-15 00:31:40 +0700

    fix(/work): чиним отображение f строки

--- bot/handlers/economy.py
+++ bot/handlers/economy.py
@@ -49,10 +49,12 @@ async def cmd_work(message: Message, bot: Bot, db: Database):
         min_income = eco_config["min_work_income"]
         max_income = eco_config["max_work_income"]
         current_income = random.randint(min_income, max_income)
-        if await db.has_valid_item(user_id, "work_tools"):
+        has_work_tools = await db.has_valid_item(user_id, "work_tools")
+
+        if has_work_tools:
             bonus = round(current_income * 0.02, 2)
             current_income = current_income + bonus
-            f"🧑‍🏭 Использование рабочих инструментов принесло вам: {bonus} {eco_config['currency_sign']}\n"
+            work_tools_msg = f"🧑‍🏭 Использование рабочих инструментов принесло вам: {bonus} {eco_config['currency_sign']}\n"
 
         new_bal = current_bal + current_income
         new_bal = round(new_bal, 2)
