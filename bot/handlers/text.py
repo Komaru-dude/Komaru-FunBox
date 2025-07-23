0361ad4be9d4c89257d7c32d7d8f29e72d9bd2fd
Komaru-dude <honkhonkovic842@gmail.com>
2025-07-23 10:20:40 +0700

    feat(репутация): больше логов

--- bot/handlers/text.py
+++ bot/handlers/text.py
@@ -199,7 +199,10 @@ async def text(message: Message, bot: Bot, state: FSMContext, db: Database):
         if not text_msg:
             return
         if await db.is_setting_enabled(chat_id, "give_random_rep"):
-            if random.random() < await db.get_setting(chat_id, "random_rep"):
+            chance = await db.get_setting(chat_id, "random_rep")
+            roll = random.random()
+            logger.debug(f"🎲 Проверка шанса: выпало {roll}, шанс {chance}")
+            if roll < chance:
                 await db.update_reputation(user1.id, chat_id, "auto_add")
                 logger.debug(f"✅ Пользователю выдана репутация: {user1.id}")
 
