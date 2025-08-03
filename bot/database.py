a78db09fcd0db58a217f3ce2d88ad7cbdeb1470b
Komaru-dude <honkhonkovic842@gmail.com>
2025-08-03 20:23:02 +0700

    style: black

--- bot/database.py
+++ bot/database.py
@@ -791,9 +791,12 @@ class Database:
         """
         await self.ensure_connection()
         async with self.pool.acquire() as conn:
-            await conn.execute("""
+            await conn.execute(
+                """
                 DELETE FROM global_users WHERE user_id = $1
-            """, user_id)
+            """,
+                user_id,
+            )
 
     async def cleanup_all_expired_items(self):
         """Очищает все просроченные предметы в инвертаре"""
