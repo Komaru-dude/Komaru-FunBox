0a6e3fd55d816f5c5be10d118bbe1192f0ba76b0
Komaru-dude <honkhonkovic842@gmail.com>
2025-09-24 20:17:25 +0700

    feat: тихое отсутствие ответа при silent = True

--- bot/filters/cooldown_filter.py
+++ bot/filters/cooldown_filter.py
@@ -6,20 +6,22 @@ from bot.utils.aio_tools import convert_seconds
 
 
 class CooldownFilter(BaseFilter):
-    def __init__(self, command: str, cooldown: int):
+    def __init__(self, command: str, cooldown: int, silent: bool = False):
         self.command = command
         self.cooldown = cooldown
+        self.silent = silent
 
     async def __call__(self, message: Message, db: Database):
         user_id = message.from_user.id
         chat_id = message.chat.id
         available = await db.is_command_available(user_id, self.command, self.cooldown)
         if not available:
-            if await db.is_setting_enabled(chat_id, "sendcooldown"):
-                cooldown_sec = await db.get_cooldown_remaining(user_id, self.command)
-                d, h, m, s = convert_seconds(cooldown_sec)
-                await message.reply(
-                    f"⏳ Команда будет доступна через: {d} дней {h} часов {m} минут {s} секунд"
-                )
+            if not self.silent:
+                if await db.is_setting_enabled(chat_id, "sendcooldown"):
+                    cooldown_sec = await db.get_cooldown_remaining(user_id, self.command)
+                    d, h, m, s = convert_seconds(cooldown_sec)
+                    await message.reply(
+                        f"⏳ Команда будет доступна через: {d} дней {h} часов {m} минут {s} секунд"
+                    )
             return False
         return True
