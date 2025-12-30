39df7eca1a900c308eba81b571248ac50b3fb694
Not-a-dude <honkhonkovic842@gmail.com>
2025-12-30 15:52:48 +0700

    style: isort + black

--- bot/handlers/text.py
+++ bot/handlers/text.py
@@ -398,8 +398,10 @@ async def text(message: Message, bot: Bot, state: FSMContext, db: Database):
             await message.reply("🪵 В лесу аукай, себе в сраку себе")
             return
 
-        if text_msg.lower() == "/бонум" and await db.is_command_available(user1.id, "bonum_ru", 5):
-                await bot.send_sticker(
+        if text_msg.lower() == "/бонум" and await db.is_command_available(
+            user1.id, "bonum_ru", 5
+        ):
+            await bot.send_sticker(
                 message.chat.id,
                 "CAACAgIAAyEFAASbCRfOAAJW2mjT7S6mjNl2eq1K3OsShmsV2K8AAzotAAIEtJhLnn7lET7JhBM2BA",
                 reply_to_message_id=message.message_id,
