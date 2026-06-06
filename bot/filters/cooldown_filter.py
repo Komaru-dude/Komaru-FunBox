450f44d6c3b747099722d3c010498773980f638e
Not-a-dude <honkhonkovic842@gmail.com>
2026-06-06 11:10:44 +0700

    feat: возможность уменьшения кулдаунов команд для премиум пользователей

--- bot/filters/cooldown_filter.py
+++ bot/filters/cooldown_filter.py
@@ -6,15 +6,23 @@ from bot.utils.aio_tools import convert_seconds
 
 
 class CooldownFilter(BaseFilter):
-    def __init__(self, command: str, cooldown: int, silent: bool = False):
+    def __init__(self, command: str, cooldown: int, reduce_for_premium: bool = False, silent: bool = False):
         self.command = command
         self.cooldown = cooldown
         self.silent = silent
+        self.reduce_for_premium = reduce_for_premium
 
     async def __call__(self, message: Message, db: Database):
         user_id = message.from_user.id
         chat_id = message.chat.id
-        available = await db.is_command_available(user_id, self.command, self.cooldown)
+        cooldown = self.cooldown
+
+        if self.reduce_for_premium:
+            is_premium = await db.is_premium_user(user_id)
+            if is_premium:
+                cooldown = self.cooldown // 2
+
+        available = await db.is_command_available(user_id, self.command, cooldown)
         if not available:
             if not self.silent:
                 if await db.is_setting_enabled(chat_id, "sendcooldown"):
